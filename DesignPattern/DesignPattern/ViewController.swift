886b2eaace4da4ee9743687fec72a753bb664b6b
KeyL <keyl_kong@163.com>
2023-05-21 10:52:43 +0800

    实现builder模式例子

---
+++ DesignPattern/DesignPattern/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  DesignPattern
+//
+//  Created by keyl on 2023/5/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
